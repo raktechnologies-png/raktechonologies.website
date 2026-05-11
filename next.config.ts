dcee775b0c0ad2428dcb75f5d42ff72b75201894
Kgoane Agreement <rak@Kgoanes-MacBook-Pro.local>
2026-05-11 12:25:01 +0200

    update website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/services",
+        destination: "/solutions",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
