4f648f9016cefbde6a440b7b616ee7209df0ae92
Kgoane Agreement <rak@Kgoanes-MacBook-Pro.local>
2026-05-11 11:28:23 +0200

    update website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${dmSans.variable} ${jakarta.variable}`} suppressHydrationWarning>
+    <head>
+     <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NSPFH9TG');</script>
+            <!-- End Google Tag Manager -->
+    </head>
+    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSPFH9TG"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
       <body className="min-h-screen antialiased" suppressHydrationWarning>
         <PageLoader />
         <CustomCursor />
